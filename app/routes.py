a2cdfb7c76079c00c6b55ab30c50e7ac00b43238
John Sachs <john.sachs@redislabs.com>
2019-06-11 21:18:02 -0700

    simplify query result access with docs_to_dict

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,17 @@ from app import app
 from flask import render_template, Flask, g
 from redisearch import Client, Query
 
+def docs_to_dict(docs):
+    reslist = []
+    for doc in docs:
+        ddict = {}
+        for field in dir(doc):
+            if field.startswith('__'):
+                continue
+            ddict.update({ field : getattr(doc, field) })
+        reslist.append(ddict)
+    return reslist
+
 @app.before_request
 def before_request():
     g.rsbeer = Client(
@@ -14,16 +25,19 @@ def before_request():
 @app.route('/')
 @app.route('/index')
 def index():
-    query = '\"lager\" +@abv:[2 7]'
+    query = '+@abv:[2 7] +@ibu:[1 +inf]'
     q = Query(query)
     result = g.rsbeer.search(q)
-    
+    res = docs_to_dict(result.docs)
+
     return render_template(
         'index.html',
         title='Home',
+        count=result.total,
+        duration=result.duration,
         rsindex=g.rsbeer.info()['index_name'],
         rsquery=q.query_string(),
-        result=result
+        result=res
     )
 
             
